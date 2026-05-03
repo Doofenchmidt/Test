41d2e1fffa7803311e3788a1dd179f4cb34bfecc
Doofenchmidt <emilliano22alfth@gmail.com>
2026-05-03 16:09:26 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+﻿function openModal(src) {
+    document.getElementById("modal").style.display = "flex";
+    document.getElementById("modalImg").src = src;
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape") {
+        closeModal();
+    }
+});
+function showCode(type) {
+    let code = "";
+
+    if (type === "html") {
+        code = `<h1>Hello World</h1>
+<p>Ini contoh HTML</p>`;
+    }
+
+    if (type === "css") {
+        code = `body {
+    background: black;
+    color: white;
+}`;
+    }
+
+    if (type === "js") {
+        code = `function hello(){
+    console.log("Hello World");
+}`;
+    }
+
+    document.getElementById("codeContent").textContent = code;
+}
